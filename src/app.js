a60ab3cf38d928a610cb6d49adcdd22799775204
els-390 <103050889+els-390@users.noreply.github.com>
2022-06-20 15:17:57 +0100

    Show current temperature

--- src/app.js
+++ src/app.js
@@ -1,5 +1,23 @@
+function displayTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let feelsLikeElement = document.querySelector("#feels-like");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  feelsLikeElement.innerHTML = Math.round(response.data.main.feels_like);
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+
 let apiKey = "747a804390da17b65aae22185c76c12b";
 
-let apiURl = `https://api.openweathermap.org/data/2.5/weather?q=Paris&appid=${apiKey}`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Paris&appid=${apiKey}&units=metric`;
 
 console.log(apiUrl);
+
+axios.get(apiUrl).then(displayTemperature);
