81d74c4c924f7a0b847f28a6146aa8f161261389
els-390 <103050889+els-390@users.noreply.github.com>
2022-06-20 17:49:10 +0100

    Remove Console.log

--- src/app.js
+++ src/app.js
@@ -23,7 +23,6 @@ function formatDate(timestamp) {
 }
 
 function displayTemperature(response) {
-  console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
@@ -43,7 +42,5 @@ function displayTemperature(response) {
 let apiKey = "747a804390da17b65aae22185c76c12b";
 
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Paris&appid=${apiKey}&units=metric`;
-
-console.log(apiUrl);
 
 axios.get(apiUrl).then(displayTemperature);
